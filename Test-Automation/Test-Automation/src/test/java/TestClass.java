360ab2082e3689776f06424c5ea7ee64457b0c81
Nehoray <135737618+nehorayshimoni@users.noreply.github.com>
2025-03-25 19:45:15 +0200

    Update TestClass.java

--- Test-Automation/Test-Automation/src/test/java/TestClass.java
+++ Test-Automation/Test-Automation/src/test/java/TestClass.java
@@ -1,6 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -14,12 +19,23 @@ public class TestClass {
         WebDriverManager.edgedriver().setup();
         driver = new EdgeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.google.com");
+        driver.get("https://www.ebay.com/");
     }
 
     @Test
-    public void openGoogle() {
+    public void getTitle() {
         System.out.println("Title: " + driver.getTitle());
+    }
+    @Test
+    public void performSearch() {
+        WebElement searchGoogle = driver.findElement(By.name("_nkw"));
+        searchGoogle.sendKeys("test search bar");
+
+        searchGoogle.sendKeys(Keys.ENTER);
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        System.out.println("New Title: " + driver.getTitle());
     }
 
     @AfterTest
